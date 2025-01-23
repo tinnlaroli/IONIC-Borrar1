a5b7f249f1b0b78bd3c457f936c673b9ec693240
Martin <olivarezmartin714@gmail.com>
2025-01-23 08:10:49 -0600

    feat : add communication between the interface and the .ts

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -7,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  email : string = '';
+  password : string = '';
+  isLogged : boolean = false;
+  
+  constructor() { 
+    //this.email = ''; <- No es necesario, es una forma de inicializar la variable
+    //this.password = ''; <- No es necesario, es una forma de inicializar la variable
+  }
 
   ngOnInit() {
   }
 
+  login(){ 
+    if(this.email === 'admin' && this.password === 'admin'){
+      this.isLogged = true;
+      alert('Login correcto');
+    } else {
+      this.isLogged = true;
+      alert('Login incorrecto');
+    } 
+  } 
 }
